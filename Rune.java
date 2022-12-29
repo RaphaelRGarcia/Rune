021595406d21f2854550c743e1c84aa10bcbd723
RaphaelRGarcia <91548880+RaphaelRGarcia@users.noreply.github.com>
2022-12-29 10:47:17 -0300

    Update Rune.java

--- Rune.java
+++ Rune.java
@@ -18,11 +18,11 @@ public class Rune {
         Scanner teclado=new Scanner(System.in);
         SenhasRune senhas= new SenhasRune();
         String entrar;
-        CriptografiaEscrita.InicializaSenha();
+        CriptografiaEscrita.InicializaSenha();//carrega a senha para memória
         System.out.println("Digite sua senha :");
-        entrar=teclado.nextLine();
-        if(CriptografiaEscrita.getLogin().equals(CriptografiaEscrita.Criptografa(entrar))){
-            senhas=CriptografiaEscrita.LeArquivo(senhas);
+        entrar=teclado.nextLine();//recebe a senha do teclado
+        if(CriptografiaEscrita.getLogin().equals(CriptografiaEscrita.Criptografa(entrar))){//compara senha de login com a entrada
+            senhas=CriptografiaEscrita.LeArquivo(senhas);//carrega as senhas dos arquivos para a memória
             int op;
             do{
                 System.out.println("Bem Vindo!");
@@ -38,27 +38,27 @@ public class Rune {
                         String nome=teclado.nextLine();
                         String pass=teclado.nextLine();
                         SenhaRune senha=new SenhaRune();
-                        senha.setSenha(pass);
-                        senhas.adicionaSenha(nome, senha.getSenha());
-                        CriptografiaEscrita.Salva(senhas);
+                        senha.setSenha(pass);//define a senha
+                        senhas.adicionaSenha(nome, senha.getSenha());//adiciona o nome e a senha na memória
+                        CriptografiaEscrita.Salva(senhas);//salva as mudanças no arquivo
                         break;
                     case 2:
                         System.out.println("Senhas registradas");
-                        senhas.verNomes();
+                        senhas.verNomes();//função que imprime os nomes
                         System.out.println("Digite o número da senha que gostaria de remover!");
-                        int passop=teclado.nextInt();
+                        int passop=teclado.nextInt();//recebe o indece da senha para ser removida
                         System.out.println("Tem certeza?\n1-Sim\n2-Outra tecla");
                         op=teclado.nextInt();
-                        if(op==1)senhas.removeSenha(passop);
-                        CriptografiaEscrita.Salva(senhas);
+                        if(op==1)senhas.removeSenha(passop);//caso seja confirmado remove a senha da memória
+                        CriptografiaEscrita.Salva(senhas);//salva mudanças em arquivo
                         break;
                     case 3:
                         System.out.println("Digite senha de login:");
                         entrar=teclado.nextLine();
-                        if(CriptografiaEscrita.getLogin().equals(CriptografiaEscrita.Criptografa(entrar))){
+                        if(CriptografiaEscrita.getLogin().equals(CriptografiaEscrita.Criptografa(entrar))){//verifica a senha com a entrada
                             if(senhas.getNumSenhas()>0){
                                 for(int i=0;i<senhas.getNumSenhas();i++){
-                                    System.out.println(senhas.getNomeInd(i)+" "+ senhas.getSenhaInd(i));
+                                    System.out.println(senhas.getNomeInd(i)+" "+ senhas.getSenhaInd(i));//imprime os nomes e suas senhas
                                 }
                             }
                             else{
