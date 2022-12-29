c321c3757089a3a8c39224586d5ed74d8c207010
RaphaelRGarcia <91548880+RaphaelRGarcia@users.noreply.github.com>
2022-12-29 10:50:58 -0300

    Update SenhasRune.java

--- SenhasRune.java
+++ SenhasRune.java
@@ -12,7 +12,7 @@ import java.io.IOException;
  *
  * @author rapha
  */
-public class SenhasRune {
+public class SenhasRune {//classe que guarda nomes, senhas e número de senhas em memória
     private ArrayList<String> senhas= new ArrayList<>();
     private ArrayList<String> nomes= new ArrayList<>();
     private int num_senhas;
@@ -20,33 +20,33 @@ public class SenhasRune {
     public SenhasRune(){
     }
     
-    public void adicionaSenha(String nome,String senha){
+    public void adicionaSenha(String nome,String senha){//função que adiciona nomes e senhas nos ArrayLists e incrementa o número de senhas
         this.nomes.add(nome);
         this.senhas.add(senha);
         num_senhas++;
     }
     
-    public void removeSenha(int pos){
+    public void removeSenha(int pos){//remove nomes e senhas dos ArrayLists e decrementa o número de senhas
         this.nomes.remove(pos);
         this.senhas.remove(pos);
         num_senhas--;
     }
 
-    public void verNomes(){
+    public void verNomes(){//imprime os nomes
         for(int i=0;i<this.senhas.size();i++){
             System.out.println(i+" "+this.nomes.get(i));
         }
     }
 
-    public int getNumSenhas(){
+    public int getNumSenhas(){//retorna o número de senhas
         return this.num_senhas;
     }
     
-    public String getSenhaInd(int index){
+    public String getSenhaInd(int index){//retorna a senha do índice
         return this.senhas.get(index);
     }
     
-    public String getNomeInd(int index){
+    public String getNomeInd(int index){//retorna o nome do índice
         return this.nomes.get(index);
     }
     
